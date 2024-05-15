efe7bcd6d0f4647a162c11302a46afb85a04d0c3
Konrad Pławik <kplawik@outlook.com>
2024-05-15 15:06:00 +0200

    Example scenarios added.

---
+++ scenarios/create_trainset.sh
@@ -0,0 +1,4 @@
+cd ../src
+python font_generator.py 100 100 25 25 times.ttf A 0 train_directory 
+python font_generator.py 100 100 25 25 times.ttf B 0 train_directory 
+python font_generator.py 100 100 25 25 times.ttf C 0 train_directory 
---
+++ scenarios/first_test.sh
@@ -0,0 +1,2 @@
+cd ../src
+python madaline_ocr.py train_directory test_directory
